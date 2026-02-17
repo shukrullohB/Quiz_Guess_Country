369a9297d3afb5b3ba0ca6b69f19a8a02e0b129c
shukrullohB <162560348+shukrullohB@users.noreply.github.com>
2026-02-17 14:49:55 +0500

    updated act

---
+++ lib/The_world/world.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class World extends StatefulWidget {
+  const World({super.key});
+
+  @override
+  State<World> createState() => _WorldState();
+}
+
+class _WorldState extends State<World> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
